1e12a5c71b4f01b732e2551e469b97bad1b0727c
ashraful2430 <www.savage582@gmail.com>
2026-04-07 14:29:40 +0600

    updated css file

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      colors: {
+        accent: "#1D9E75",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
